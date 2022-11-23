9000198dfbbf8891830a039980ec94887d8b5abb
momonote2 <yiyi6464@naver.com>
2022-11-23 23:12:43 +0900

    abc

---
+++ Ex06_Model/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,50 @@
+package com.study.springboot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+	
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception{
+		return " Model and view";
+	}
+	
+	@RequestMapping("/test1")
+	public String test1(Model model) {
+		//Model 객체이용해서 view로 데이터전달
+		model.addAttribute("name","홍길동");
+		
+		return "test1";
+	}
+	
+	@RequestMapping("/mv")
+	public ModelAndView test2() {
+		//데이터와 뷰를 동시에 설정이 가능
+		ModelAndView mv = new ModelAndView();
+		
+		List<String> list = new ArrayList<>();
+		list.add("test1");
+		list.add("test2");
+		list.add("test3");
+		
+		//jstl로 호출 
+		mv.addObject("lists",list);
+		mv.addObject("ObjectTest","테스트입니다.");
+		mv.addObject("name","홍길동");
+		mv.setViewName("view/myView");
+		
+		return mv;
+	}
+	
+	
+	
+}
