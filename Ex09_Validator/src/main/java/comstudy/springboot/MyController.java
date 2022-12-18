94abaec2eab7bfc92c257953e70bd14b004e2f64
momonote2 <yiyi6464@naver.com>
2022-12-18 20:49:38 +0900

    12-18

--- Ex09_Validator/src/main/java/comstudy/springboot/MyController.java
+++ Ex09_Validator/src/main/java/comstudy/springboot/MyController.java
@@ -1,5 +1,40 @@
 package comstudy.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MyController {
 
+	
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception{
+		return " Validator (1) ";
+	}
+	
+	@RequestMapping("/insertForm")
+	public String insert1() {
+		return "createPage";
+	}
+	
+	@RequestMapping("/create")
+	public String insert2(@ModelAttribute("dto") ContentDto contentDto, BindingResult result) {
+		String page = "createPAge";
+		System.out.println(contentDto);
+		
+		ContentValidator validator = new ContentValidator();
+		validator.validate(contentDto, result);
+		if(result.hasErrors()) {
+			page = "createPage";
+		}
+		
+		return page;
+	}
+
+	
+	
+	
 }
